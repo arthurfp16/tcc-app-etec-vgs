0d9e8e1e01e5c25baf797f723ca328b8f35f3d29
arthurfp16 <82186586+arthurfp16@users.noreply.github.com>
2022-04-12 18:19:39 -0300

    feat: adiciona arquivo do componente PostCardStyle e seu estilo

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,11 @@
+import { PostCardContent, PostCardTitle } from '../styles/PostCardStyle'
+
+
+export const PostCard = (img, author, title, text, date) => {
+    return (
+        <PostCardContent>
+        {/* img */ }
+        <PostCardTitle>Post Card test</PostCardTitle>
+        </PostCardContent>
+    )
+}
---
+++ src/styles/PostCardStyle.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const PostCardContent = styled.article`
+    width: 50%;
+    background-color: #F1F1F1; /* change for an external theme */
+`
+
+export const PostCardTitle = styled.h2`
+    text-align: center;
+    font-size: 12px;
+    font-weight: bold;
+    width: 100%;
+`
