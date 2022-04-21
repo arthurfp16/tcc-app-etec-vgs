dace6019e2e2f83db488f3a2215554069fc68528
arthurfp16 <82186586+arthurfp16@users.noreply.github.com>
2022-04-20 22:04:34 -0300

    feat: adiciona pagina sobre mobile

--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -1,8 +1,12 @@
 import { Header } from "../components/Header"
 import { MotivationalPhrase } from "../components/motivationalPhrase"
 import Head from 'next/head'
-import { SectionWithImage } from "../components/SectionWithImage"
+import { ImageContainer } from "../components/SectionWithImage"
 import { MainStyled } from "../styles/MainStyled"
+import { Footer } from "../components/Footer"
+
+import img1 from '../../public/img1.png'
+
 export default function About() {
   return (
     <>
@@ -14,24 +18,23 @@ export default function About() {
       <MotivationalPhrase />
 
       <MainStyled>
-        <SectionWithImage>
+        <ImageContainer imgSrc={img1} >
           O Cerrado é um bioma de formações vegetais constituído de uma união de paisagens que passam entre ecossistemas campestres, ciliares, florestais e rupestres. Possuindo uma grande biodiversidade animal e vegetal.
-        </SectionWithImage>
+        </ImageContainer>
 
-        <SectionWithImage>
+        <ImageContainer imgSrc={img1} containerFlexDirection='row-reverse'>
           A flora conta com aproximadamente 13 mil espécies, cada uma com  suas próprias características.
-        </SectionWithImage>
+        </ImageContainer>
 
-        <SectionWithImage>
+        <ImageContainer imgSrc={img1} containerFlexDirection='column-reverse'> 
           Em relação a fauna, o cerrado é reconhecido pela riquíssima concentração de mamíferos de médio porte, aves e incetos.
-        </SectionWithImage>
+        </ImageContainer>
 
         <p>
           Com tudo isso o cerrado é repleto de interações estas comprendem desde as relações alimentares, medicina popular, religiosas, habitações(antigamente), conhecimentos tradicionais(cultura), e identidade para com a paisagem.
         </p>
-
       </MainStyled>
-
+      <Footer />
     </>
   )
 }
