2791d913e4d3e9659a46b9b6fe653203cc6df834
arthurfp16 <82186586+arthurfp16@users.noreply.github.com>
2022-04-27 16:10:05 -0300

    feat: adiciona a comunicaçao da pagina com o backend

--- src/components/CreationMenuForm.jsx
+++ src/components/CreationMenuForm.jsx
@@ -1,80 +1,73 @@
-import styled from "styled-components"
 import Image from "next/image"
 import uploadIcon from '../../public/upload.svg'
-
-
-
-const Form = styled.form`
-    width: 100%;
-    height: fit-content;
-    padding: 26px 36px;
-    text-align: center;
-    margin: 41px auto auto auto;
-    background-color: ${props => props.theme.colors.lightGreen};
-    box-shadow: 2px 4px 4px #00000040;
-    max-width: 308px;
-
-    div{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        gap: 41px;
-        margin-top: 26px;
-    }
-`
-
-const Label = styled.label`
-    display: flex;
-    flex-direction: column;
-    align-items: stretch;
-    text-align: start;
-    margin-bottom: 10px;
-`
-
-const Button = styled.button`
-    width: 92px;
-    height: ${props => props.variant == 'upload' ? '26px ' : '36px'};
-    box-shadow: 2px 4px 4px #00000040;
-    background-color:  ${props => props.variant == 'upload' ? props.theme.colors.darkGreen : props.theme.colors.lightOrange};
-    border: none;
-    outline: none;
-    color: ${props => props.theme.colors.white};
-    align-self: ${props => props.variant == 'upload' ? 'flex-end' : null};
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 6px;
-`
-
-const TextArea = styled.textarea`
-    resize: vertical;
-    overflow:hidden;
-`
-
-function textAreaAdjust(element) {
-    element.style.height = "1px";
-    element.style.height = (25 + element.scrollHeight) + "px"
-}
+import { useState } from "react"
+import { Form, Label, Button, TextArea, textAreaAdjust } from '../styles/components/CreationMenuFormStyle'
 
 export const CreationMenuForm = () => {
+    const [titleValue, setTitleValue] = useState()
+    const [textValue, setTextValue] = useState()
+    const [althorValue, setAlthorValue] = useState()
+    const [errors, setErrors] =  useState()
+    const validateData = () => {
+        if(titleValue.length > 50){
+            setErrors('titulo muinto longo')
+        }
+    }
+    const sendData = async () => {
+        await fetch('http://localhost:3000/api/post', {
+            method: 'POST',
+            body: [titleValue, textValue, althorValue]
+        })
+    }
+
     return (
-        <Form>
-            <Label htmlFor="title">
+        <Form onSubmit={(event) => {
+            event.preventDefault()
+            sendData()
+        }}>
+
+            <Label htmlFor="title" >
                 Titulo:
-                <input type="text" id="title" />
+                <input
+                    onKeyDown={(event) => event.key != 'Enter' ? null : event.preventDefault()}
+                    value={titleValue} onChange={(event) => setTitleValue(event.target.value)}
+                    type="text"
+                    id="title"
+                     />
             </Label>
+
             <Label htmlFor="title">
                 Texto:
-                <TextArea onKeyUp={(event) => textAreaAdjust(event.target)} type="text" id="title" />
+                <TextArea
+                    value={textValue}
+                    onChange={(event) => {
+                        textAreaAdjust(event.target)
+                        setTextValue(event.target.value)
+                    }
+                    }
+                    type="text"
+                    id="title" />
             </Label>
+
             <Label htmlFor="title">
                 Autor:
-                <input type="text" id="title" />
+                <input
+                    value={althorValue}
+                    onChange={(event) => setAlthorValue(event.target.value)}
+                    onKeyDown={(event) => event.key != 'Enter' ? null : event.preventDefault()}
+                    type="text"
+                    id="title" />
             </Label>
+
             <div>
-                <Button variant='upload'> <Image src={uploadIcon} /> Upload</Button>
-                <Button >Publicar</Button>
+                <Button variant='upload' ><Image src={uploadIcon} /> Upload</Button>
+                <Button
+                    type='submit'
+                    onKeyDown={(event) => event.key != 'Enter' ? null : event.preventDefault()}>
+                    Publicar
+                </Button>
             </div>
+
         </Form>
     )
 }
--- src/pages/api/post.js
+++ src/pages/api/post.js
@@ -5,6 +5,9 @@ async function handler(req, res) {
         if(req.method == 'GET') {
             const dbPostData = await prisma.posts.findMany()
             res.status(200).json(dbPostData)
+        }else if (req.method == 'POST'){
+            console.log(req.body)
+            res.status(200).json('ok')
         }
     }
     catch (err) {
