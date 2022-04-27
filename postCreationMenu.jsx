50095983a1d580b6a477c8ccd911dfbf3e368ef6
Arthur Fabris <82186586+arthurfp16@users.noreply.github.com>
2022-04-26 21:20:39 -0300

    feat: add  estilo do menu de criaçao

---
+++ postCreationMenu.jsx
@@ -0,0 +1,15 @@
+import { Header } from "../components/Header"
+import { Footer } from "../components/Footer.jsx"
+import { CreationMenuForm } from "../components/CreationMenuForm.jsx"
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <CreationMenuForm />
+            <Footer />
+        </>
+    )
+}
+
+export default Home
