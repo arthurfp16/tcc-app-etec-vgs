3ff141f74d6f5445bd6ba7458cc14a2445399177
Arthur Fabris <82186586+arthurfp16@users.noreply.github.com>
2022-04-26 21:28:02 -0300

    fix: tenta corrigir os ultimos commits

--- src/pages/postCreationMenu.jsx
+++ src/pages/postCreationMenu.jsx
@@ -1,10 +1,12 @@
 import { Header } from "../components/Header"
 import { Footer } from "../components/Footer.jsx"
+import { CreationMenuForm } from "../components/CreationMenuForm.jsx"
 
 const Home = () => {
     return (
         <>
             <Header />
+            <CreationMenuForm />
             <Footer />
         </>
     )
