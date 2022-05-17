348ec417af9942e8974eab7cd6730ff9f7f98fc7
arthurfp16 <82186586+arthurfp16@users.noreply.github.com>
2022-05-17 18:47:25 -0300

    fix: pagina sobre

--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -4,32 +4,68 @@ import Image from 'next/image'
 import styled from 'styled-components'
 
 import img1 from '../../public/img1.png'
-import img2 from '../../public/img2.png'
+import img2 from "../../public/img2.png"
 import img3 from '../../public/img3.png'
+
+const AboutMain = styled.main`
+  display: grid;
+  gap: 42px;
+  padding-top: 42px;
+
+  .image-container {
+    display: flex;
+    gap: 20px;
+    flex-direction: column;
+    position: relative;
+    height: fit-content;
+    width: 100%;
+    .image {
+      position: relative !important;
+
+      border-radius: 2.7px;
+    }
+    
+  } 
+
+  @media screen and (min-width: 360px) {
+    .image-container--rigth {
+      flex-direction: row-reverse;
+      p {
+        width: 50%;
+      }
+    }
+    .image-container--left {
+      flex-direction: row;
+      p {
+        width: 50%;
+      }
+    }
+  }
+`
 
 export default function About() {
   return (
     <>
-      <Header linkHref='/posts' linkText='Ler artigos mais recentes' />
-      <main>
+      <Header linkText='Ler artigos mais recentes' />
+      <AboutMain>
 
         <div className="image-container image-container--left">
-          <Image src={img1} alt="" />
+          <Image class='image' objectFit="contain" src={img1} alt="" />
           <p>
             O Cerrado é um bioma de formações vegetais constituído de uma união de paisagens que passam entre ecossistemas campestres, ciliares, florestais e rupestres.
           </p>
         </div>
 
         <div className="image-container image-container--rigth">
-          <Image src={img1} alt="" />
+          <Image class='image' objectFit="contain" src={img2} alt="" />
           <p>
             Possuindo uma grande biodiversidade animal e vegetal.
             A flora conta com aproximadamente 13 mil espécies, cada uma com  suas próprias características.
           </p>
         </div>
 
-        <div className="image-container image-container--top">
-          <Image src={img1} alt="" />
+        <div className="image-container">
+          <Image class='image' objectFit="contain" src={img3} alt="" />
           <p>
             Em relação a fauna, o cerrado é reconhecido pela riquíssima concentração de mamíferos de médio porte, aves e incetos.
           </p>
@@ -39,7 +75,7 @@ export default function About() {
           Com tudo isso o cerrado é repleto de interações estas comprendem desde as relações alimentares, medicina popular, religiosas, habitações(antigamente), conhecimentos tradicionais(cultura), e identidade para com a paisagem.
         </p>
 
-      </main>
+      </AboutMain>
       <Footer />
     </>
   )
